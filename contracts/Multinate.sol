366011c562e2e434cc400d456d2114fdf87b547a
Jun Meng <110725797+junmtan@users.noreply.github.com>
2023-03-20 18:54:51 +0800

    feat: :sparkles: Added initial logic for attestation scoring

--- contracts/Multinate.sol
+++ contracts/Multinate.sol
@@ -5,6 +5,25 @@ import "./interfaces/IAttestationStation.sol";
 
 contract Multinate {
     IAttestationStation public attestationStation;
+    uint256 public minimumAttestationScore;
+
+    bytes32 constant REGISTRATION_KEY = keccak256("registration");
+    bytes32 constant TAX_EXEMPT_STATUS_KEY = keccak256("tax_exempt_status");
+    bytes32 constant FINANCIAL_STATEMENTS_KEY = keccak256("financial_statements");
+    bytes32 constant MISSION_STATEMENT_KEY = keccak256("mission_statement");
+
+    mapping(address => uint256) public charityScores;
+
+    event CharityEligibilityUpdated(address indexed charity, bool eligible);
+
+    constructor(address _attestationStation, uint256 _minimumAttestationScore) {
+        attestationStation = IAttestationStation(_attestationStation);
+        minimumAttestationScore = _minimumAttestationScore;
+    }
+
+    function setMinimumAttestationScore(uint256 _minimumAttestationScore) external {
+        minimumAttestationScore = _minimumAttestationScore;
+    }
 
     constructor(address _attestation) {
         attestationStation = IAttestationStation(_attestation);
