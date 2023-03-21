b36b013bba0c1e5a1a9a2c315cc288c8f355a918
Jun Meng <110725797+junmtan@users.noreply.github.com>
2023-03-21 14:33:31 +0800

    feat: :sparkles: Added create campaign function

--- contracts/Multinate.sol
+++ contracts/Multinate.sol
@@ -2,18 +2,39 @@
 pragma solidity 0.8.15;
 
 import "./interfaces/IAttestationStation.sol";
+import "hardhat/console.sol";
 
 contract Multinate {
     IAttestationStation public attestationStation;
     uint256 public minimumAttestationScore;
 
-    bytes32 constant REGISTRATION_KEY = keccak256("registration");
-    bytes32 constant TAX_EXEMPT_STATUS_KEY = keccak256("tax_exempt_status");
-    bytes32 constant FINANCIAL_STATEMENTS_KEY = keccak256("financial_statements");
-    bytes32 constant MISSION_STATEMENT_KEY = keccak256("mission_statement");
+    bytes32 public constant REGISTRATION_KEY = keccak256("registration");
+    bytes32 public constant TAX_EXEMPT_STATUS_KEY = keccak256("tax_exempt_status");
+    bytes32 public constant FINANCIAL_STATEMENTS_KEY = keccak256("financial_statements");
+    bytes32 public constant MISSION_STATEMENT_KEY = keccak256("mission_statement");
 
     mapping(address => uint256) public charityScores;
+    struct Campaign {
+        address payable charity;
+        string title;
+        string description;
+        uint256 targetAmount;
+        uint256 currentAmount;
+        uint256 deadline;
+        bool active;
+    }
 
+    uint256 public nextCampaignId = 1;
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    event CampaignCreated(
+        uint256 indexed campaignId,
+        address indexed charity,
+        string title,
+        uint256 targetAmount,
+        uint256 deadline
+    );
     event CharityEligibilityUpdated(address indexed charity, bool eligible);
 
     constructor(address _attestationStation, uint256 _minimumAttestationScore) {
@@ -31,24 +52,24 @@ contract Multinate {
         // Add a scoring system based on the attestation data
         bytes memory registrationData = attestationStation.attestations(msg.sender, _charity, REGISTRATION_KEY);
         score += calculateRegistrationScore(registrationData);
-
+        console.log("score after registration: %s", score);
         bytes memory taxExemptStatusData = attestationStation.attestations(msg.sender, _charity, TAX_EXEMPT_STATUS_KEY);
         score += calculateTaxExemptStatusScore(taxExemptStatusData);
-
+        console.log("score after tax exempt: %s", score);
         bytes memory financialStatementsData = attestationStation.attestations(
             msg.sender,
             _charity,
             FINANCIAL_STATEMENTS_KEY
         );
         score += calculateFinancialStatementsScore(financialStatementsData);
-
+        console.log("score after financial statement: %s", score);
         bytes memory missionStatementData = attestationStation.attestations(
             msg.sender,
             _charity,
             MISSION_STATEMENT_KEY
         );
         score += calculateMissionStatementScore(missionStatementData);
-
+        console.log("score after mission statement: %s", score);
         charityScores[_charity] = score;
         bool isEligible = score >= minimumAttestationScore;
         emit CharityEligibilityUpdated(_charity, isEligible);
@@ -81,7 +102,7 @@ contract Multinate {
         if (_financialStatementsData.length > 0) {
             // In this example, we assign a score between 0 and 25 based on the financial transparency level
             // (e.g., percentage of funds spent on the mission, administrative costs, etc.)
-            uint8 transparencyLevel = uint8(_financialStatementsData[0]);
+            uint256 transparencyLevel = uint8(_financialStatementsData[0]);
             return (transparencyLevel * 25) / 100;
         }
         return 0;
@@ -91,9 +112,35 @@ contract Multinate {
         // Check if mission statement data is available
         if (_missionStatementData.length > 0) {
             // In this example, we assign a score between 0 and 25 based on the mission's alignment with global goals (e.g., UN SDGs)
-            uint8 alignmentScore = uint8(_missionStatementData[0]);
+            uint256 alignmentScore = uint8(_missionStatementData[0]);
             return (alignmentScore * 25) / 100;
         }
         return 0;
+    }
+
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        uint256 _targetAmount,
+        uint256 _deadline
+    ) external {
+        require(isCharityEligible(msg.sender), "The charity organization is not eligible");
+        require(_deadline > block.timestamp, "Deadline must be in the future");
+        require(_targetAmount > 0, "Target amount must be greater than zero");
+
+        uint256 campaignId = nextCampaignId;
+        nextCampaignId++;
+
+        campaigns[campaignId] = Campaign({
+            charity: payable(msg.sender),
+            title: _title,
+            description: _description,
+            targetAmount: _targetAmount,
+            currentAmount: 0,
+            deadline: _deadline,
+            active: true
+        });
+
+        emit CampaignCreated(campaignId, msg.sender, _title, _targetAmount, _deadline);
     }
 }
